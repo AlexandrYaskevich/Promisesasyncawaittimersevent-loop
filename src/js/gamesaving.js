3f12acb3fa64835fa75efe7010df1d43a5ba4dea
Alexandr Yaskevich <yask.alexandr@gmail.com>
2024-05-29 22:59:36 +0300

    promise second work

--- src/js/gamesaving.js
+++ src/js/gamesaving.js
@@ -4,16 +4,15 @@ import json from './parser';
 export default class GameSavingLoader {
   static load() {
     return new Promise((resolve) => {
-        return resolve(true);
+      const data = read();
+      data.then((response) => {
+        const value = json(response);
+        return value;
+        }).then((value) => {
+          resolve (value);
+        }).catch((error) => {
+          throw new Error(error);
+        });
     });
   }
 }
-
-GameSavingLoader.load().then(data => {
-    const data = read();
-    return data;
-})
-    .then(value => {
-        const value = json(data);
-        return value;
-    });
