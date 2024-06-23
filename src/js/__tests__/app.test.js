0fd8c4c01a938f971e8cbd1eaf21c6201f5b32e7
Alexandr Yaskevich <yask.alexandr@gmail.com>
2024-06-23 18:09:20 +0300

    add test OK

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,17 +1,24 @@
 import GameSavingLoader from "../app";
 
 jest.setTimeout(5000);
-test('test app fail', async(done) =>{   
-    expect(await GameSavingLoader.load()).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
-    done();
+test('test app fail', async() =>{ 
+  const data = {
+    id: 9,
+    created: 1546300800,
+    userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 }
+  };
+  const result = await GameSavingLoader.load();
+    expect(result).toEqual(data);;
+    
 });
 
 
 test('test app fail error', async () => {
-        expect.assertions(1);
-        try {
-            await GameSavingLoader.load();
-        } catch (error) {
-          expect(await GameSavingLoader.load(132)).rejects.toMatch('error');
-        }
-      });
+  GameSavingLoader.load = jest.fn().mockRejectedValue(new Error('Test error'));
+
+  try {
+    await GameSavingLoader.load();
+  } catch (error) {
+    expect(error.message).toBe('Test error');
+  }
+}, 10000);
